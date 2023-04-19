e5b0ba394fb579635194b55a54dd27e8d9cbbbd1
bollampr <122890217+bollampr@users.noreply.github.com>
2023-04-18 22:23:06 -0400

    final

---
+++ js/script.js
@@ -0,0 +1,114 @@
+document.getElementById("img1").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML  = '<iframe id="21" style="border-radius:12px" src="https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+document.getElementById("img2").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/17l7MIu0Jh0tdgK7or9ovw?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+document.getElementById("img3").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML  = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/3EyDuWSBOZGl5CAlBkSrOE?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+document.getElementById("img4").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML  = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/6e8eJS3DUWZ9SAjHE5AD5m?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+document.getElementById("img5").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML  = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/3DGQ1iZ9XKUQxAUWjfC34w?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+document.getElementById("img6").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML  = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/4SZko61aMnmgvNhfhgTuD3?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+document.getElementById("img7").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML  = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/0fEO7g2c5onkaXsybEtuD2?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+document.getElementById("img8").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML  = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/7dAm8ShwJLFm9SaJ6Yc58O?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+document.getElementById("img9").addEventListener("click", function(){
+
+    clicked = true
+    console.log('here')
+    const player = document.createElement("player");
+    // player.innerHTML  = "<iframe title = 'kendrick album' style='border-radius:12px' src='https://open.spotify.com/embed/album/007DWn799UWvfY1wwZeENR?utm_source=generator' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>"
+    player.innerHTML  = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/79dL7FLiJFOO0EoehUHQBv?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    
+    document.getElementById("musicplayer").appendChild(player)
+    console.log('done')
+
+})
+
+
+
+
+
+
